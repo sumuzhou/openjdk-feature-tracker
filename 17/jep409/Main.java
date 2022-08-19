ff652f31c83c192f9019b97383215ed8d9aaa647
sumuzhou <ggyoung16@gmail.com>
2022-08-19 16:44:07 +0800

    git helper function

--- 17/jep409/Main.java
+++ 17/jep409/Main.java
@@ -4,6 +4,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello JEP409");
+        log("Hello JEP409");
+    }
+
+    private static void log(String message, Object ... args) {
+        System.out.printf(message, args);
     }
 }
