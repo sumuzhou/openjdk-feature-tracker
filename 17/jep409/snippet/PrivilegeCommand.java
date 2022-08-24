046e5fe3a7bb5d4a6f91aefa6d5aebbe47c186b2
sumuzhou <ggyoung16@gmail.com>
2022-08-24 16:45:52 +0800

    change Command back to interface

--- 17/jep409/snippet/PrivilegeCommand.java
+++ 17/jep409/snippet/PrivilegeCommand.java
@@ -1,4 +1,4 @@
-public sealed class PrivilegeCommand extends Command permits InfraCommand {
+public sealed class PrivilegeCommand implements Command permits InfraCommand {
     public void executeMe() {
         System.out.println(":_ Sounds cool to have some privileges.\n");
     }
--- 17/jep409/snippet/SuperUserCommand.java
+++ 17/jep409/snippet/SuperUserCommand.java
@@ -1,4 +1,4 @@
-public final class SuperUserCommand extends Command {
+public final class SuperUserCommand implements Command {
     public void executeMe() {
         System.out.println(":) I'm the super user and I can do whatever I want.\n");
     }
