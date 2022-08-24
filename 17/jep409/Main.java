18870b9d5987f290a33ff814aa3f45d73a9178a0
sumuzhou <ggyoung16@gmail.com>
2022-08-24 16:58:28 +0800

    add content for reflection api

--- 17/jep409/Main.java
+++ 17/jep409/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 
 //JAVA 17+
@@ -13,9 +15,15 @@ public class Main {
         log("PrivilegeCommand can have limited extentions, but they must declared by definition.\n");
         executeCommand(new PrivilegeCommand());
 
-        log("NormalCommand can be extended freely, but all the descendants are of type NormalCommand, not Command.\n");
+        log("NormalCommand can be extended freely as an ordinary class.\n");
         executeCommand(new NormalCommand());
-        log("An interesting fact: the keyword `non-sealed` is the first **hyphenated keyword** introduced in Java.\n");
+        log("An interesting fact: the keyword `non-sealed` is the first **hyphenated keyword** introduced in Java.\n\n");
+
+        log("There are two reflection APIs added in order to retrieve info of sealed class:\n");
+        log("> Class<?>[] getPermittedSubclasses() - ");
+        log("The permitted subclasses of Command are: %s\n", Arrays.toString(Command.class.getPermittedSubclasses()));
+        log("> boolean isSealed() - ");
+        log("Is the Command sealed? %s\n", Command.class.isSealed());
     }
 
     private static void log(String message, Object ... args) {
